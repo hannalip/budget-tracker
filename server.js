97ac3e2a952053b7974ff2134ac5a4d0cde299f2
Hannali Patidar <hannalip@gmail.com>
2020-04-11 13:11:32 -0400

    url

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const MONGODB_URI = process.env.MONGODB_URI  || 'mongodb://localhost/BudgetTracker';
+const MONGODB_URI = process.env.MONGODB_URI  || 'mongodb://user1:password1@ds263248.mlab.com:63248/heroku_r5h9d7l3';
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
